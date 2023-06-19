cd0b3faf67383c9e23d0abff69f9a6e6ed79f9b6
shannon <shantsou03@gmail.com>
2023-06-19 14:26:10 +0800

    Add frame to game page

--- src/components/BackButton.js
+++ src/components/BackButton.js
@@ -2,7 +2,7 @@ import { useNavigate } from "react-router-dom";
 import { styled } from "@mui/system";
 import { Button, Typography } from "@mui/material";
 
-export default function StartButton() {
+export default function BackButton() {
     const ShadowButton = styled(Button)({
         position: 'relative',
         '&::before': {
@@ -28,15 +28,15 @@ export default function StartButton() {
         <ShadowButton
         size="large"
         sx={{ bgcolor: "rgba(255, 0, 153, 0.3)",
-                border: "3px solid", 
-                borderColor: "#FF97D5", 
-                borderRadius: "10px",
-                color: "#FF97D5",
-                '&:hover': {
-                bgcolor: "rgba(255, 0, 153, 0.8)",
-                border: "3px solid",
-                borderRadius: "10px",
-                } }}
+              border: "3px solid", 
+              borderColor: "#FF97D5", 
+              borderRadius: "10px",
+              color: "#FF97D5",
+              '&:hover': {
+              bgcolor: "rgba(255, 0, 153, 0.8)",
+              border: "3px solid",
+              borderRadius: "10px",
+              } }}
         type="submit"
         variant="outlined"
         onClick={handleStartClick}
--- src/components/GamePage.js
+++ src/components/GamePage.js
@@ -1,4 +1,4 @@
-import { Typography } from "@mui/material";
+import { Box, Grid, Typography } from "@mui/material";
 import { styled } from "@mui/system";
 import BackButton from "./BackButton";
 
@@ -6,7 +6,8 @@ const NeonText = styled(Typography)({
   textShadow: "0 0 8px #FF0099, 0 0 10px #fff, 0 0 12px #FF0099, 0 0 14px #FF0099, 0 0 16px #FF0099, 0 0 18px #FF0099",
   color: "#FF97D5",
   fontFamily: "'Oswald', sans-serif",
-  marginBottom: "1rem"
+  marginBottom: "1rem",
+  textAlign: "center"
 });
 
 export default function GamePage() {
@@ -24,10 +25,25 @@ export default function GamePage() {
         height: '100vh',
         width: '100vw',
       }}>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          height: "90%",
+          width: "80%",
+          padding: "1rem",
+          backgroundColor: "rgba(0, 0, 0, 0.8)", // Adjust the background color and opacity
+          border: "2px solid #FF0099",
+          borderRadius: "8px", // Adjust the border radius as per your needs
+        }}>
         <NeonText variant="h6">
             GAME GOES HERE
         </NeonText>
-        <BackButton />
+        <Grid container justifyContent="center">
+            <BackButton />
+        </Grid>
+      </Box>
     </div>
   );
 }
